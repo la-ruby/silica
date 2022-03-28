59583d5ab905cb9d3950085debd2fecaac9ce901
Sam W <sam@sam-we.com>
2022-03-28 16:39:16 -0700

    Cleanup

--- test/jobs/cache_contact_count_job_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class CacheContactCountJobTest < ActiveJob::TestCase
-  test "#perform" do
-    stub_sendgrid_marketing_lists
-    CacheContactCountJob.perform_now
-  end
-end
-
