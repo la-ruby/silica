d0ba60745d0c10e014ff8b7644f74596c09f6d61
Robbert van Elk <robbert_vanelk@hotmail.com>
2022-04-02 11:26:31 +0200

    Rubocop warnings for addendum versions controller

--- app/controllers/addendum_versions_controller.rb
+++ app/controllers/addendum_versions_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Addendum Versions Controller
 class AddendumVersionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_addendum_version, only: %i[update]
@@ -6,15 +9,12 @@ class AddendumVersionsController < ApplicationController
 
   def update
     authorize @addendum_version
-    streams = saves
-    streams = send_addendum_version_to_seller if params[:what_was_clicked] == 'send_addendum_version_to_seller'
-    streams = sign_addendum_version if params[:what_was_clicked] == 'sign_addendum_version'
-    streams = discard_addendum_version if params[:what_was_clicked] == 'discard_addendum_version'
+    streams = clicked_streams(params[:what_was_clicked])
 
     respond_to do |format|
-       format.turbo_stream do
-         render turbo_stream: streams
-       end
+      format.turbo_stream do
+        render turbo_stream: streams
+      end
     end
   end
 
@@ -23,12 +23,12 @@ class AddendumVersionsController < ApplicationController
     authorize nil, policy_class: AddendumVersionPolicy
     project = Project.find(params[:project_id])
     Addendum.add_an_addendum(project)
-
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Added' }),
-          turbo_stream.replace('apollo-underwriting-review-offer-pane', partial: '/component/underwriting_review_offer_pane', locals: { project_id: project.id })
+          turbo_stream.replace('apollo-underwriting-review-offer-pane',
+                               partial: '/component/underwriting_review_offer_pane', locals: { project_id: project.id })
         ]
       end
     end
@@ -36,61 +36,98 @@ class AddendumVersionsController < ApplicationController
 
   private
 
-    def set_addendum_version
-      @addendum_version = AddendumVersion.find(params[:id])
+  def clicked_streams(what_was_clicked)
+    case what_was_clicked
+    when 'send_addendum_version_to_seller'
+      send_addendum_version_to_seller
+    when 'sign_addendum_version'
+      sign_addendum_version
+    when 'discard_addendum_version'
+      discard_addendum_version
+    else
+      saves
     end
+  end
 
-    # Only allow a list of trusted parameters through.
-    def addendum_version_params
-      params.require(:addendum_version).permit(:expiration, :deadline, :terms) if  params[:addendum_version].present?
+  def set_addendum_version
+    @addendum_version = AddendumVersion.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def addendum_version_params
+    params.require(:addendum_version).permit(:expiration, :deadline, :terms) if params[:addendum_version].present?
+  end
+
+  def saves
+    if params[:addendum_version].present?
+      @addendum_version.update(terms: addendum_version_params[:terms])
+      @addendum_version.update(expiration: begin
+        DateTime.strptime(addendum_version_params[:expiration],
+                          '%Y-%m-%dT%R').iso8601
+      rescue StandardError
+        nil
+      end)
+      @addendum_version.update(deadline: begin
+        DateTime.strptime(addendum_version_params[:deadline],
+                          '%Y-%m-%dT%R').iso8601
+      rescue StandardError
+        nil
+      end)
     end
+    [turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Saved' }),
+     turbo_stream.replace(
+       "addendum-#{@addendum_version.addendum.id}-addendum-version-#{@addendum_version.id}-panel",
+       partial: '/component/addendum_version', locals: {
+         addendum_version_id: @addendum_version.id, editable: true
+       }
+     )]
+  end
 
-    def saves
-      if params[:addendum_version].present?
-        @addendum_version.update(terms: addendum_version_params[:terms])
-        @addendum_version.update(expiration: (DateTime.strptime(addendum_version_params[:expiration],'%Y-%m-%dT%R').iso8601 rescue nil))
-        @addendum_version.update(deadline: (DateTime.strptime(addendum_version_params[:deadline],'%Y-%m-%dT%R').iso8601 rescue nil))
-      end
-      [ turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Saved' }),
-        turbo_stream.replace("addendum-#{@addendum_version.addendum.id}-addendum-version-#{@addendum_version.id}-panel", partial: '/component/addendum_version', locals: { addendum_version_id: @addendum_version.id, editable: true  })
-      ]
-
-    end
-
-    def send_addendum_version_to_seller
-      sendgrid_message_id = AddendumMail.new.perform(
-        to: @addendum_version.addendum.project.email,
-        mop_token: @addendum_version.addendum.project.repc.mop_token,
-        name: @addendum_version.addendum.project.name,
+  def send_addendum_version_to_seller
+    sendgrid_message_id = AddendumMail.new.perform(
+      to: @addendum_version.addendum.project.email,
+      mop_token: @addendum_version.addendum.project.repc.mop_token,
+      name: @addendum_version.addendum.project.name,
+      street: @addendum_version.addendum.project.street
+    )
+    if @addendum_version.addendum.project.dual?
+      AddendumMail.new.perform(
+        to: @addendum_version.addendum.project.secondEmail,
+        mop_token: @addendum_version.addendum.project.repc.second_seller_mop_token,
+        name: @addendum_version.addendum.project.secondName,
         street: @addendum_version.addendum.project.street
       )
-      if @addendum_version.addendum.project.dual?
-        AddendumMail.new.perform(
-          to: @addendum_version.addendum.project.secondEmail,
-          mop_token: @addendum_version.addendum.project.repc.second_seller_mop_token,
-          name: @addendum_version.addendum.project.secondName,
-          street: @addendum_version.addendum.project.street
-        )
-      end
-      @addendum_version.addendum.project.update(sendgrid_message_id: sendgrid_message_id)
-      @addendum_version.update(sent_to_seller_at: Time.now.iso8601)
-      [
-        turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Sent!' }),
-        turbo_stream.replace("addendum-#{@addendum_version.addendum.id}-addendum-version-#{@addendum_version.id}-panel", partial: '/component/addendum_version', locals: { addendum_version_id: @addendum_version.id, editable: true  }),
-      ]
     end
+    @addendum_version.addendum.project.update(sendgrid_message_id: sendgrid_message_id)
+    @addendum_version.update(sent_to_seller_at: Time.now.iso8601)
+    [
+      turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Sent!' }),
+      turbo_stream.replace(
+        "addendum-#{@addendum_version.addendum.id}-addendum-version-#{@addendum_version.id}-panel",
+        partial: '/component/addendum_version', locals: {
+          addendum_version_id: @addendum_version.id, editable: true
+        }
+      )
+    ]
+  end
 
-    def sign_addendum_version
-      [
-        turbo_stream.replace('apollo-redirect', partial: '/redirect', locals: { url: @addendum_version.addendum.project.create_docusign_envelope_for_addendum_version(@addendum_version.id) })
-      ]
-    end
+  def sign_addendum_version
+    [
+      turbo_stream.replace('apollo-redirect',
+                           partial: '/redirect', locals: { url:
+                             @addendum_version.addendum.project
+                             .create_docusign_envelope_for_addendum_version(@addendum_version.id) })
+    ]
+  end
 
-    def discard_addendum_version
-      new_addendum_version = @addendum_version.addendum.add_an_addendum_version
-      [
-        turbo_stream.replace('apollo-underwriting-review-offer-pane', partial: '/component/underwriting_review_offer_pane', locals: { project_id: @addendum_version.addendum.project.id }),
-        turbo_stream.replace('flashes', partial: '/flashes', locals: { message: "Added draft #{new_addendum_version.version}" })
-      ]
-    end
+  def discard_addendum_version
+    new_addendum_version = @addendum_version.addendum.add_an_addendum_version
+    [
+      turbo_stream.replace('apollo-underwriting-review-offer-pane',
+                           partial: '/component/underwriting_review_offer_pane',
+                           locals: { project_id: @addendum_version.addendum.project.id }),
+      turbo_stream.replace('flashes', partial: '/flashes',
+                                      locals: { message: "Added draft #{new_addendum_version.version}" })
+    ]
+  end
 end
