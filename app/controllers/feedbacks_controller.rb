206bb2a478df40c1cd76dc7a65f9798abc0ad992
Robbert van Elk <robbert_vanelk@hotmail.com>
2022-04-02 12:10:00 +0200

    Rubocop fixes for feedbacks controller

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
+# Feedbacks Controller
 class FeedbacksController < ApplicationController
   before_action :set_record, only: %i[show new create]
   before_action :set_area_offer
+  before_action :authorize_feedback_policy
   after_action :verify_authorized
 
-  # GET /examples/1 or /examples/1.json
-  def show
-    authorize nil, policy_class: FeedbackPolicy
-  end
+  def show; end
 
-  # GET /examples/new
-  def new
-    authorize nil, policy_class: FeedbackPolicy
-  end
+  def new; end
 
-  # POST /examples or /examples.json
   def create
-    authorize nil, policy_class: FeedbackPolicy
-
-    @mbo_request.second_seller_mode? ? @record.update(second_seller_rejected_feedback: rejected_feedback) : @record.update(rejected_feedback: rejected_feedback)
+    if @mbo_request.second_seller_mode?
+      @record.update(second_seller_rejected_feedback: rejected_feedback)
+    else
+      @record.update(rejected_feedback: rejected_feedback)
+    end
     respond_to do |format|
       format.turbo_stream do
         redirect_to request.referrer.gsub(%r{/new\z}, ''), notice: 'Feedback submitted'
@@ -29,9 +26,12 @@ class FeedbacksController < ApplicationController
 
   private
 
+  def authorize_feedback_policy
+    authorize nil, policy_class: FeedbackPolicy
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_record
-    # @example = Example.find(params[:id])
     @mbo_request = MboRequest.new(token: params[:token])
     @record = @mbo_request.record
   end
