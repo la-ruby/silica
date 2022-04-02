163553ff552b413ef1aa7bb21a3ec37c7a6e1bc9
Robbert van Elk <robbert_vanelk@hotmail.com>
2022-04-02 12:41:42 +0200

    Rubcop fixes for Intake form completions controller

--- app/controllers/intake_form_completions_controller.rb
+++ app/controllers/intake_form_completions_controller.rb
@@ -5,37 +5,43 @@ class IntakeFormCompletionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resources
   before_action :set_area_backend
+  before_action :authorize_intake_form_completion_policy
   after_action :verify_authorized
 
   # POST /examples or /examples.json
   def create
-    authorize nil, policy_class: IntakeFormCompletionPolicy
     mark_as_complete
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Updated' }),
-          turbo_stream.replace('underwriting-intake-form-pane', partial: '/underwriting_intake_form_panes/underwriting_intake_form_pane', locals: { project_id: @project.id })
-        ]
+        render turbo_stream: turbo_stream_array(turbo_stream)
       end
     end
   end
 
   # DELETE /examples/1 or /examples/1.json
   def destroy
-    authorize nil, policy_class: IntakeFormCompletionPolicy
     undo_mark_as_complete
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Updated' }),
-          turbo_stream.replace('underwriting-intake-form-pane', partial: '/underwriting_intake_form_panes/underwriting_intake_form_pane', locals: { project_id: @project.id })
-        ]
+        render turbo_stream: turbo_stream_array(turbo_stream)
       end
     end
   end
 
   private
+
+  def authorize_intake_form_completion_policy
+    authorize nil, policy_class: IntakeFormCompletionPolicy
+  end
+
+  def turbo_stream_array(turbo_stream)
+    [
+      turbo_stream.replace('flashes', partial: '/flashes', locals: { message: 'Updated' }),
+      turbo_stream.replace('underwriting-intake-form-pane',
+                           partial: '/underwriting_intake_form_panes/underwriting_intake_form_pane',
+                           locals: { project_id: @project.id })
+    ]
+  end
 
   def set_resources
     @project = Project.find params[:project_id]
