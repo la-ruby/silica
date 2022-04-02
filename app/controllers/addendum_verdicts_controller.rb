4502b8cd008ae4e99349b8257843a2a54948ecb4
Robbert van Elk <robbert_vanelk@hotmail.com>
2022-04-02 11:09:19 +0200

    Rubocop warnings addendum verdicts controller

--- app/controllers/addendum_verdicts_controller.rb
+++ app/controllers/addendum_verdicts_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Addendum Verdicts Controller
 class AddendumVerdictsController < ApplicationController
   include DocuSign::Mixin
   before_action :set_area_offer
@@ -6,24 +9,29 @@ class AddendumVerdictsController < ApplicationController
   # POST /examples or /examples.json
   def create
     authorize nil, policy_class: AddendumVerdictPolicy
-
     @addendum_version = AddendumVersion.find params[:addendum_version_id]
     @repc = Repc.find(@addendum_version.related_repc_id)
     @second_seller_mode = (params[:second_seller_mode] == 'true')
-    @url = ''
-    process_rejection if params[:action_button] == 'reject'
-    process_acceptance if params[:action_button] == 'sign_via_docusign'
-    
+    @url = addendum_url(params[:action_button])
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.replace('apollo-redirect', partial: '/redirect', locals: { url: @url })
-        ]
+        render turbo_stream: [turbo_stream.replace('apollo-redirect', partial: '/redirect', locals: { url: @url })]
       end
     end
   end
 
   private
+
+  def addendum_url(action_button)
+    case action_button
+    when 'reject'
+      process_rejection
+    when 'sign_via_docusign'
+      process_acceptance
+    else
+      ''
+    end
+  end
 
   def process_rejection
     if @second_seller_mode
@@ -40,26 +48,25 @@ class AddendumVerdictsController < ApplicationController
   end
 
   def process_acceptance
-    if @second_seller_mode
-      @url = create_recipient_view(
-        envelope_id: @addendum_version.docusign_envelope_id,
-        return_url: "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.second_seller_mop_token}",
-        role: :second_seller,
-        name: @repc.project.secondName,
-        email: @repc.project.secondEmail
-      )
-      encoded = "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.second_seller_mop_token}"
-      @url = "#{APOLLO_CDN}/bounceV10.html?a=#{encoded}" if APOLLO_INTERNAL_PRODUCTION
-    else
-      @url = create_recipient_view(
-        envelope_id: @addendum_version.docusign_envelope_id,
-        return_url: "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.mop_token}",
-        role: :seller,
-        name: @repc.project.name,
-        email: @repc.project.email
-      )
-      encoded = "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.mop_token}"
-      redirect = "#{APOLLO_CDN}/bounceV10.html?a=#{encoded}" if APOLLO_INTERNAL_PRODUCTION
-    end
+    @url = if @second_seller_mode
+             set_url(@addendum_version.docusign_envelope_id,
+                     "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.second_seller_mop_token}", :second_seller,
+                     @repc.project.secondName, @repc.project.secondEmail)
+           else
+             set_url(@addendum_version.docusign_envelope_id, "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@repc.mop_token}",
+                     :seller, @repc.project.name, @repc.project.email)
+           end
+    enc = "#{APOLLO_PORTAL_FULL_DOMAIN}/offer/#{@second_seller_mode ? @repc.second_seller_mop_token : @repc.mop_token}"
+    @url = "#{APOLLO_CDN}/bounceV10.html?a=#{enc}" if APOLLO_INTERNAL_PRODUCTION
+  end
+
+  def set_url(envelope_id, return_url, role, name, email)
+    create_recipient_view(
+      envelope_id: envelope_id,
+      return_url: return_url,
+      role: role,
+      name: name,
+      email: email
+    )
   end
 end
