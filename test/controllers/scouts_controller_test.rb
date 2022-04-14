efb491f612d22764a98fc9de5f1e2d26371eed88
Sam W <sam@sam-we.com>
2022-04-13 19:09:13 -0700

    Fix situation where a test was making network requests

--- test/controllers/scouts_controller_test.rb
+++ test/controllers/scouts_controller_test.rb
@@ -7,9 +7,10 @@ class ScoutsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'post scouts_path' do
-    VCR.use_cassette('gitignored', :record => :all) do
+    stub_request(:post, 'https://example.com/testing')
+    #VCR.use_cassette('gitignored', :record => :all) do
       post scouts_path, params: {scout: {project_id: @project.id}}, headers: { accept: Mime[:turbo_stream].to_s }
       assert_equal "200", response.code
-    end
+    #end
   end
 end
