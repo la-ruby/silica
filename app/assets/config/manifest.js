ef874603dc037311fdd7258614b1521eb23df1f7
Sam W <sam@sam-we.com>
2022-04-15 12:34:24 -0700

    Rectify styles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,11 @@
+//= link actiontext.scss
+//= link sessions.css
+//= link colors.css
+//= link font_size.css
+//= link font_size-i.css
+//= link debug.css
+//= link silica.css
+
 //= link_tree ../images
 // link_directory ../stylesheets .css
 // link_directory ../javascripts .js
