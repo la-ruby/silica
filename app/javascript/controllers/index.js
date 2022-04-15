5d424548b95b23bc599b43b3144d6b7881eb5bb9
Sam W <sam@sam-we.com>
2022-04-15 13:52:20 -0700

    Fix for Module not found: Error

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,101 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import AcceptanceController from "./acceptance_controller.js"
+application.register("acceptance", AcceptanceController)
+
+import ActsLikeLinkController from "./acts_like_link_controller.js"
+application.register("acts-like-link", ActsLikeLinkController)
+
+import AgreedToTermsController from "./agreed_to_terms_controller.js"
+application.register("agreed-to-terms", AgreedToTermsController)
+
+import ApolloTooltipController from "./apollo_tooltip_controller.js"
+application.register("apollo-tooltip", ApolloTooltipController)
+
+import BasicFlatpickr2Controller from "./basic_flatpickr_2_controller.js"
+application.register("basic-flatpickr-2", BasicFlatpickr2Controller)
+
+import BasicFlatpickrController from "./basic_flatpickr_controller.js"
+application.register("basic-flatpickr", BasicFlatpickrController)
+
+import CityFilterController from "./city_filter_controller.js"
+application.register("city-filter", CityFilterController)
+
+import ContactsController from "./contacts_controller.js"
+application.register("contacts", ContactsController)
+
+import CycloramaController from "./cyclorama_controller.js"
+application.register("cyclorama", CycloramaController)
+
+import DispositionsPrepareListingPaneController from "./dispositions_prepare_listing_pane_controller.js"
+application.register("dispositions-prepare-listing-pane", DispositionsPrepareListingPaneController)
+
+import DzoneController from "./dzone_controller.js"
+application.register("dzone", DzoneController)
+
+import FadeOutController from "./fade_out_controller.js"
+application.register("fade-out", FadeOutController)
+
+import GeographicAddressCompleterController from "./geographic_address_completer_controller.js"
+application.register("geographic-address-completer", GeographicAddressCompleterController)
+
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
+
+import InputInlineController from "./input_inline_controller.js"
+application.register("input-inline", InputInlineController)
+
+import ListingsController from "./listings_controller.js"
+application.register("listings", ListingsController)
+
+import MapController from "./map_controller.js"
+application.register("map", MapController)
+
+import MoneyController from "./money_controller.js"
+application.register("money", MoneyController)
+
+import OpensDialoggController from "./opens_dialogg_controller.js"
+application.register("opens-dialogg", OpensDialoggController)
+
+import PopoverController from "./popover_controller.js"
+application.register("popover", PopoverController)
+
+import PropertyAnalysisController from "./property_analysis_controller.js"
+application.register("property-analysis", PropertyAnalysisController)
+
+import PropertyDispositionChecklistController from "./property_disposition_checklist_controller.js"
+application.register("property-disposition-checklist", PropertyDispositionChecklistController)
+
+import RedirectController from "./redirect_controller.js"
+application.register("redirect", RedirectController)
+
+import ReloadController from "./reload_controller.js"
+application.register("reload", ReloadController)
+
+import SelectInlineController from "./select_inline_controller.js"
+application.register("select-inline", SelectInlineController)
+
+import ShareController from "./share_controller.js"
+application.register("share", ShareController)
+
+import SortProjectsController from "./sort_projects_controller.js"
+application.register("sort-projects", SortProjectsController)
+
+import SourceFilterController from "./source_filter_controller.js"
+application.register("source-filter", SourceFilterController)
+
+import StateFilterController from "./state_filter_controller.js"
+application.register("state-filter", StateFilterController)
+
+import StatusFilterController from "./status_filter_controller.js"
+application.register("status-filter", StatusFilterController)
+
+import SubmitsFormDelayedController from "./submits_form_delayed_controller.js"
+application.register("submits-form-delayed", SubmitsFormDelayedController)
+
+import SubmitsFormImmediatelyController from "./submits_form_immediately_controller.js"
+application.register("submits-form-immediately", SubmitsFormImmediatelyController)
+
+import TextareaInlineController from "./textarea_inline_controller.js"
+application.register("textarea-inline", TextareaInlineController)
