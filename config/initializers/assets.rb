fd536b17cfd07c37314fbaff627486e94d02a30e
Sam W <sam@sam-we.com>
2022-04-15 09:47:59 -0700

    Tweaks to ./bin/rails css:install:bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,10 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# ./bin/rails css:install:bootstrap added this
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+# left here as a reference, one can import e.g. flatpickr assets with this technique
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
