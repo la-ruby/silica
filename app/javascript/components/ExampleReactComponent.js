d60507032f262b805c6aab297846357bbbe7eebb
Sam W <sam@sam-we.com>
2022-04-18 09:19:49 -0700

    Handle domcontentloaded events

--- app/javascript/components/ExampleReactComponent.js
+++ app/javascript/components/ExampleReactComponent.js
@@ -3,11 +3,23 @@ import { FullFileBrowser } from 'chonky';
 
 
 class ExampleReactComponent extends React.Component {
+
+
   render() {
+    const files = [
+      { id: 'lht', name: 'Projects', isDir: true },
+      {
+        id: 'mcd',
+        name: 'chonky-sphere-v2.png',
+        thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
+      },
+    ]
+    const folderChain = [{ id: 'xcv', name: 'Demo', isDir: true }]
+
     return (
       <div className="experiment1650288360">
 	<div>{this.props.exampleprop}</div>
-	<FullFileBrowser files={[]} folderChain={[]} />
+	<FullFileBrowser files={[]} folderChain={folderChain} />
       </div>
     );
   }
