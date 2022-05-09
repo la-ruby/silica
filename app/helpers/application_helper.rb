8cbf203d7bfaba97f4464d48598751f11c1d1474
Sam W <sam@sam-we.com>
2022-05-09 08:06:15 -0700

    One Toast appears

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -153,8 +153,4 @@ module ApplicationHelper
     end
     (crumbs.join(' / ') + ' /').html_safe
   end
-
-  def svg_box
-    '<svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#007aff"></rect></svg>'.html_safe
-  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   # Definitions, activity here stands refers to the activity feed tab
   DEFINITIONS = [
-    { 'category' => 'project_creation',                   'toast' => '1', 'toast_stength' => '100', 'activity' => '1', 'animation' => '1', 'animation_strength' => '100', 'animation_phase_in_timesan' => nil, 'animation_phase_in_start' => nil, 'voice' => 'en_US', 'voice_strength' => '100', 'voice_phase_in_timespan' => nil, 'voice_phase_in_start' => nil },
+    { 'category' => 'project_creation',                   'color' => 'primary', 'toast' => '1', 'toast_stength' => '100', 'activity' => '1', 'animation' => '1', 'animation_strength' => '100', 'animation_phase_in_timesan' => nil, 'animation_phase_in_start' => nil, 'voice' => 'en_US', 'voice_strength' => '100', 'voice_phase_in_timespan' => nil, 'voice_phase_in_start' => nil },
   ]
 
   # blog.corsego.com/turbo-hotwire-broadcasts
@@ -9,14 +9,29 @@ class Event < ApplicationRecord
     if activityble?
       broadcast_prepend_to("backend_regular_user_activity_#{record.id}_stream", target: "activity_#{record.id}", partial: "events/activity", locals: { event: self })
     end
+    if toastable?
+      broadcast_prepend_to('backend_regular_user_toast_stream', target: 'silica-toasts', partial: "events/toast", locals: { event: self  })      
+    end
   end
 
   def record
     record_type.constantize.find(record_id)
   end
 
+  def definition
+    DEFINITIONS.select{|x| x['category'] == category }.first
+  end
+
   def activityble?
-    DEFINITIONS.select{|x| x['category'] == category }.first['activity'] == '1'
+    definition['activity'] == '1'
+  end
+
+  def toastable?
+    definition['activity'] == '1'
+  end
+
+  def color
+    "text-#{definition['color']}"
   end
 
   def inventor
