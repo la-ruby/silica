686d57fdb1e325e8da6fef8487a463654a319acf
Sam W <sam@sam-we.com>
2022-05-09 08:31:14 -0700

    Animation happens

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -3,5 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     new bootstrap.Toast(this.element, {}).show()
+    if ( $(this.element).data('confetti') == '1' ) {
+      setTimeout(function() {
+        confetti({
+          angle: 180,
+          spread: randomInRange(50, 70),
+          particleCount: randomInRange(50, 100),
+          origin: { y: 0, x: 1 },
+          zIndex: 6000
+        });
+      }, 200)
+    }
   }
 }
